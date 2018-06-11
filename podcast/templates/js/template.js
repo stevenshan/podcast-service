bf2c29fc58035309acc07e65fc5504eeeec4e3e6
Steven Shan <steven.shan99@gmail.com>
2018-06-10 23:39:25 -0400

    added minimizing search bar

--- podcast/templates/js/template.js
+++ podcast/templates/js/template.js
@@ -8,6 +8,7 @@ $(function(){ // on page load
     -------------------------------------------
     */
 
+    // display first three links by default
     if ($("#settingsMenu #navMenu li").length > 3)
     {
         var count = 0;
@@ -22,56 +23,72 @@ $(function(){ // on page load
 
         /* minimize menu if anything else is clicked */
 
+        // cancel progation to ignore click on element
         $("#settingsMenu #navMenuToggle").click(function(e){
             e.stopPropagation();
             if (!$("#settingsMenu #navMenu").hasClass("expanded"))
             {
                 $("#settingsMenu #navMenu").addClass("expanded");
+                $("#searchBar").addClass("minimized");
             }
         });
 
+        // essentially event for losing "focus" on menu bar
         $("body").click(function(){
             if ($("#settingsMenu #navMenu").hasClass("expanded"))
             {
                 $("#settingsMenu #navMenu").removeClass("expanded");
+                $("#searchBar").removeClass("minimized");
             }
         });
     }
 
     /*
     -------------------------------------------
-    Search bar events
+    Search bar focus events
     -------------------------------------------
     */
 
-    // elem parameter is #searchInput object
-    var focusOutEvent = function(elem) {
-        elem.removeClass("focused");
+    var focusOutEvent = function() {
+        $("header #searchBar").removeClass("focused");
+        $("#searchBarToggleStatus").prop("checked", false);
     };
-    $("#searchInput")
+
+    $("header #searchInput")
         .focus(function(){ // on focus event
-            $(this).addClass("focused");
+            $("header #searchBar").addClass("focused");
         })
         .focusout(function(){ // when focus leaves
             if ($(this).val() == "") // if search bar is empty
             {
-                var elem = this;
                 window.setTimeout(function() {
-                    focusOutEvent($(elem));
-                }, 50);
+                    focusOutEvent();
+                }, 100);
             }
         });
 
     $("#searchInput + label").click(function(){
-        var input = $("#searchInput");
-        if (input.hasClass("focused")) {
+        var input = $("header #searchInput");
+        if ($("header #searchBar").hasClass("focused")) {
             input.val("");
-            focusOutEvent(input);
+            focusOutEvent();
         }
         else {
             input.focus();
-            console.log("focus");
         }
+    });
+
+    /*
+    -------------------------------------------
+    Search bar minimization
+    -------------------------------------------
+    */
+
+    // focus on search bar when maximizing search bar
+    $("#searchBarToggle").click(function(e){
+        window.setTimeout(function(){
+            $("#searchInput").focus();
+        }, 50);
     });
 
 });
